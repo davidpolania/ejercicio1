e752d2b5995615e8b915f1a4ee2707128c6dbaca
USUARIO <USUARIO@PC19>
2018-04-04 21:32:33 -0500

    inicio de proyecto

--- src/ejercicio1/Ejercicio1.java
+++ src/ejercicio1/Ejercicio1.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio1;
 
-/**
- *
- * @author USUARIO
- */
+
 public class Ejercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int numero;
+        numero = 1;
+        System.out.println("el numero es: "+1);
     }
     
 }
